2a34f55338f6556a4f05f35e1d8e2baa6d47d0e9
aryands14 <100413893+aryands14@users.noreply.github.com>
2022-11-18 17:10:42 +0100

    update CSVTable.java

--- src/CSVTable.java
+++ src/CSVTable.java
@@ -6,11 +6,16 @@ import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 
 public class CSVTable extends JFrame {
+	/**
+	 * 
+	 */
+	
+	private static final long serialVersionUID = 1L;
 	static JTable table;
 	static DefaultTableModel model;
 	static JButton logButton, importButton;
 	static JTextField text;
-
+	
 
 	public CSVTable(String title, String source) {
 		super(title);
@@ -20,8 +25,6 @@ public class CSVTable extends JFrame {
 		JPanel buttonPanel = new JPanel();
 		importButton = new JButton("Importar csv");
 		buttonPanel.add(importButton);
-		logButton = new JButton("Generar Log");
-		buttonPanel.add(logButton);
 		text = new JTextField();
 
 		getContentPane().add(scroll, BorderLayout.CENTER);
@@ -31,53 +34,48 @@ public class CSVTable extends JFrame {
 	}
 	
 
-	public static void main(String args[]) {
-		CSVTable frame = new CSVTable("Tabla estudiantes", "datos-estudiantes-pevau.csv");
-		frame.setVisible(true);
-		
-		importButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-            	boolean error = false;
-     
-                String filePath = "datos-estudiantes-pevau.csv";
-                try {
-                    BufferedReader br = new BufferedReader(new InputStreamReader(
-                            new FileInputStream(filePath), "UTF-8"));
-                    String firstLine = br.readLine().trim();
-                    //String[] columnsName = firstLine.split(";");
-                    String[] columnsName = { "Nombre", "Apellido", "Materia", "Instituto" };
-                    DefaultTableModel model = (DefaultTableModel)table.getModel();
-                    model.setColumnIdentifiers(columnsName);
-                    
-                    Object[] tableLines = br.lines().toArray();
-                    
-                    for(int i = 0; i < tableLines.length; i++)
-                    {
-                    	boolean errorLinea = false;
-                        String line = tableLines[i].toString().trim();
-                        String[] dataRow = line.split(";");
-                        for(String s : dataRow) {
-                        	if (s.isEmpty()) {
-                        		text.setText("Error en la importacion ");
-                        		error = true;
-                        		errorLinea = true;
-                        	}
-                        }
-                        String[] row = {dataRow[1], dataRow[2], dataRow[5], dataRow[0]};
-                        if(!errorLinea) {
-                            model.addRow(row);
-                        } 
-                    }
-                } catch (IOException ex) {
-                	ex.printStackTrace();
+    public static void generarTabla() {
+    	boolean error = false;
+
+        String filePath = "datos-estudiantes-pevau.csv";
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(
+                    new FileInputStream(filePath), "UTF-8"));
+            
+            String firstLine = br.readLine().trim();
+            String[] columnsName = { "Nombre", "Apellido", "Materia", "Instituto" };
+            DefaultTableModel model = (DefaultTableModel)table.getModel();
+            model.setColumnIdentifiers(columnsName);
+            
+            BD miBD = new BD();
+            Object[] tableLines = br.lines().toArray();
+            for(int i = 0; i < tableLines.length; i++)
+            {
+            	boolean errorLinea = false;
+                String line = tableLines[i].toString().trim();
+                String[] dataRow = line.split(";");
+                for(String s : dataRow) {
+                	if (s.isEmpty()) {
+                		error = true;
+                		errorLinea = true;
+                	}
                 }
-                if(!error) {
-                	text.setText("Importaci�n correcta");
+                String[] row = {dataRow[1], dataRow[2], dataRow[5], dataRow[0]};
+               
+                if(!errorLinea) {
+                    model.addRow(row);
+                } else {
+            		text.setText("Error en la importacion en la línea " + dataRow[1] + " " + dataRow[2] + " " + dataRow[0] +"\n");
                 }
-                table.setAutoCreateRowSorter(true); 
             }
-		});
-		
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	}
+        } catch (IOException ex) {
+        	ex.printStackTrace();
+        }
+        if(!error) {
+        	text.setText("IMPORTACIÓN REALIZADA CON ÉXITO");
+        }
+        table.setAutoCreateRowSorter(true); 
+    }
+
+	
 }
