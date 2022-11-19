5e6bf4c13c20383cac58c31bd6de8a115bfab2d8
aryands14 <100413893+aryands14@users.noreply.github.com>
2022-11-19 19:00:29 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,14 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 public class Main {
 
 	public static void main(String[] args) {
-		HomePage mp = new HomePage("Página principal");
+		HomePage mp = new HomePage("PÃ¡gina principal");
 		mp.setVisible(true);
+		
 		HomePage.generarCSVButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				CSVTable frame = new CSVTable("Importar CSV", "datos-estudiantes-pevau.csv");
@@ -19,6 +21,23 @@ public class Main {
 				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			}
 		});
+		
+		HomePage.importarSedesButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				TablaSedes frame = new TablaSedes("Importar Sedes");
+				frame.setVisible(true);
+				TablaSedes.importarSedesButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						TablaSedes.ImportarSedes();
+					}
+				});
+				TablaSedes.removeButton.addActionListener(new ActionListener() {
+			          public void actionPerformed(ActionEvent ae) {
+			        	  TablaSedes.borrarSeleccionado();
+			          }});
+			}
+		});
+		
 		mp.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
